9626158819b393142b629226b771a8386598506e
nawafnaofal <nawafnaufal1@gmail.com>
2022-10-27 16:49:23 +0700

    praktikum IV

--- nawaf_django/blog/models.py
+++ nawaf_django/blog/models.py
@@ -1,3 +1,20 @@
+
+import email
 from django.db import models
+from django.utils.text import slugify 
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255,
+    validators = [])
+    body = models.TextField()
+    email = models.EmailField(blank=True)
+    slug = models.SlugField(blank=True, editable=False)
+    
+    def save(self):
+        self.slug = slugify(self.title)
+        super(Post,self).save()
+    # def __str__(self):
+    #     return "{}".format(self.title)
+    
+    
--- nawaf_django/blog/views.py
+++ nawaf_django/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render (request,'blog/index.html')
+    db = Post.objects.all()
+    context = {
+        'title':'Blog',
+        'heading':'Blog',
+        'subheading':'postingan',
+        'post': db,
+    }
+    return render (request, 'blog/index.html', context)
